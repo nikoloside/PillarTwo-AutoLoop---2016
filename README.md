3d3d3908745f07afbad15f0066d244ccab919607
Lab\nikol <nikoloside@gmail.com>
2018-03-06 17:11:48 +0900

    AutoLoop Google Chrome Extension Tool

---
+++ README.md
@@ -0,0 +1,16 @@
+#構造を説明する。
+
+popup.htmlを開くことによって、
+
+main.jsが実行
+
+background.js との連携によって
+
+common.jsに発信をもとめる
+
+#変更の手順
+
+main.jsでpopup.htmlの変更を制御する
+
+main.jsで発生した変更は　まずbackground.jsのデータベースに送信
+送信完了後、折り返しのpopup.htmlの変更をbackground.jsから発信
---
+++ js/config.js
@@ -0,0 +1,4 @@
+//config リクエストアワー2016
+
+var config_url =  "https://member.akb48.co.jp/logout.php";
+var config_array_proc = [{"tar":"","sel":"0","arc":""},{"tar":"login_id","sel":"2","arc":""},{"tar":"password","sel":"3","arc":"10030212"},{"tar":"login","sel":"4","arc":""},{"tar":"","sel":"1","arc":"https://member.akb48.co.jp/profile/index.php"},{"tar":"","sel":"7","arc":"AKB48公式サイト｜二本柱の会 | 会員情報詳細"},{"tar":"","sel":"1","arc":"https://member.akb48.co.jp//logout.php"}];
